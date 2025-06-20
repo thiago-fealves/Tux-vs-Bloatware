843d62638af582ab63305e9fa07c84dc045ca2f4
369gabriel <villedwhite@gmail.com>
2025-06-20 10:40:02 -0300

    mais documentacao(tamo documentando tamo documentando)

--- src/interface.cpp
+++ src/interface.cpp
@@ -2,14 +2,37 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/color.h>
+/**
+ * @brief Coordinates constructor
+ *
+ * @param x x coordinates in the display
+ * @param y y coordinates in the display
+ * @param width region width 
+ * @param heigth region heigth
+ */
 
 Coordinates::Coordinates(double x, double y, double width, double heigth) :
     _x(x), _y(y), _width(width), _heigth(heigth)
 {}
 
+
+/**
+ * @brief Button constructor
+ *
+ * @param coords button dimensions using the Coordinates class 
+ * @param color allegro-typed rgb color
+ * @param text button center text
+ * @param font center text font
+ * @param drawBackground bool that sets if the background will be displayed
+ */
 Button::Button(Coordinates coords, ALLEGRO_COLOR color, std::string text, ALLEGRO_FONT* font, bool drawBackground)
 : _coords(coords), _color(color), _text(text), _font(font),_drawBackground(drawBackground)
 {}
+
+
+/**
+ * @brief Draws the button using its attributes
+ */
 
 void Button::drawButton() {
     //precisei mudar para os botoes ficarem com cores diferentes 
@@ -26,18 +49,30 @@ void Button::drawButton() {
     / 2, ALLEGRO_ALIGN_CENTER, _text.c_str());
 }
 
-// Receives mouse coordinates and verify if it matches
-// the button coordinates.
-// mx and my = mouse x and y;
+/**
+ * @brief checks if the mouse coordinates (when click occurs) matches
+ * the button region
+ *
+ * @param mx mouse x coordinates
+ * @param my mouse y coordinates
+ */
 bool Button::gotClicked(int mx, int my) {
     return(mx >= _coords._x && mx <=_coords._x+_coords._width && my >= _coords._y && my <= _coords._y+_coords._heigth);
 
 }
 
+/**
+ * @brief set a new text to the button
+ *
+ * @param txt new text to substitute the old one
+ */
 void Button::setText(const char* txt) {
     this->_text = txt;
 }
 
+/**
+ * @brief Construct the main menu interface using pre-setted buttons
+ */
 Interface::Interface(ALLEGRO_FONT* font) :
     playButton(Coordinates(300, 300, 200, 100), al_map_rgb(50, 50, 50), "PLAY", font),
     stopSongButton(Coordinates(740, 10, 50, 50), al_map_rgb(50, 50, 50), "󰕾", font),
@@ -45,6 +80,9 @@ Interface::Interface(ALLEGRO_FONT* font) :
     exitGameButton(Coordinates(10, 540, 50, 50), al_map_rgb(50, 50, 50), "", font)
 {}
 
+/**
+ * @brief Draws the main menu interface
+ */
 void Interface::drawOffGameInterface() {
     playButton.drawButton();
     stopSongButton.drawButton();
