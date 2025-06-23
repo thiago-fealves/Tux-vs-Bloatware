2f55c75ac99d00155264e3de97ef5bddb21ded19
Arthur Cordeiro <arthurcordeirofer@gmail.com>
2025-06-22 23:30:59 -0300

    add consts

--- include/bootstrap.hpp
+++ include/bootstrap.hpp
@@ -23,6 +23,8 @@
 constexpr float FPS = 30; 
 constexpr int SCREEN_W = 800; 
 constexpr int SCREEN_H = 600;
+constexpr int BUTTON_W = 270; 
+constexpr int BUTTON_H = 70;
 constexpr int OBSTACLES_LIST_NUM = 6;
 constexpr float SCALE_PIPES = 0.33f;
 constexpr float SCALE_ASTEROID = 0.19f;
